72c7cf8bdaa23c9c25a724ded32ef0c2173ec3bc
KodaiTakao <takaokodai@team-lab.com>
2016-12-11 12:16:11 +0900

    Renamed variables

--- Assets/Fluid3D_Fast/SPH3D.cs
+++ Assets/Fluid3D_Fast/SPH3D.cs
@@ -8,40 +8,35 @@ namespace SPH3D_FAST {
         const int SIMULATION_BLOCK_SIZE = 32;
         public ComputeShader particleCS;
 
-        ComputeBuffer particleBufferRead;
-        ComputeBuffer particleBufferWrite;
+        ComputeBuffer ParticleBufferRead;
+        ComputeBuffer ParticleBufferWrite;
         ComputeBuffer GridBufferRead;
         ComputeBuffer GridBufferWrite;
 
-        public int numParticles;
-
         // ---------- For SPH ------------
-        public float SPH_RESTDENSITY = 600.0f;
-        public float SPH_INTSTIFF = 3.0f; // 堅さ(?)
-        public float SPH_PMASS = 0.00020543f; // 粒子質量
-        public float SPH_SIMSCALE = 0.004f; // シミュレートするスケール
-        public float H = 0.01f; // 有効半径(CELL_WIDTH)
-        public static float PI = 3.141592653589793f;
-        public float DT = 0.004f; // ΔT
-        public float SPH_VISC = 0.2f; // 粘性
-        public float SPH_LIMIT = 200.0f; // 速度制限
-        public float SPH_RADIUS = 0.004f; // 半径
-        public float SPH_EPSILON = 0.00001f; // 許容誤差
-        public float SPH_EXTSTIFF = 10000.0f;   // 壁の反発力
-        public float SPH_EXTDAMP = 256.0f;
-        public Vector3 MIN = new Vector3(0.0f, 0.0f, -10.0f);
-        public Vector3 MAX = new Vector3(20.0f, 50.0f, 10.0f);
-        public Vector3 GRAVITY = new Vector3(0, -10, 0);
-        private float Poly6Kern; // Poly6Kernel関数の定数
-        private float SpikyKern; // SpikyKernel関数の定数
-        private float LapKern; // LaplacianKernel関数の定数(Viscosity)
+        public int numParticles;
+        public float restDensity = 600.0f;
+        public float pressureStiff = 3.0f; // 堅さ(?)
+        public float p_mass = 0.00020543f; // 粒子質量
+        public float simulationScale = 0.004f; // シミュレートするスケール
+        public float smoothStep = 0.01f; // 有効半径(CELL_WIDTH)
+        public float dt = 0.004f; // ΔT
+        public float viscosity = 0.2f; // 粘性
+        public float vel_limit = 200.0f; // 速度制限
+        public float radius = 0.004f; // 半径
+        public float wallStiff = 10000.0f;   // 壁の反発力
+        public float restitution = 256.0f;
+        public Vector3 min = new Vector3(0.0f, 0.0f, 0.0f);
+        public Vector3 max = new Vector3(50.0f, 50.0f, 50.0f);
+        public Vector3 gravity = new Vector3(0, -10, 0);
+        private float densityCoef;
+        private float gradPressureCoef;
+        private float lapViscosityCoef;
 
         public Vector3 GridDim = new Vector3(20.0f, 50.0f, 10.0f);
         // ---------- For SPH ------------
 
-        void OnDestroy() {
-            ReleaseBuffer();
-        }
+        
 
         void Start() {
             InitializeVariables();
@@ -49,7 +44,7 @@ namespace SPH3D_FAST {
         }
 
         public ComputeBuffer GetParticleBuffer() {
-            return this.particleBufferRead;
+            return this.ParticleBufferRead;
         }
 
         public int GetMaxParticleNum() {
@@ -60,65 +55,63 @@ namespace SPH3D_FAST {
             int threadGroupSize = Mathf.CeilToInt(numParticles / SIMULATION_BLOCK_SIZE) + 1;
 
             particleCS.SetInt("_NumParticles", numParticles);
-            particleCS.SetFloat("SPH_RESTDENSITY", SPH_RESTDENSITY);
-            particleCS.SetFloat("SPH_INTSTIFF", SPH_INTSTIFF);
-            particleCS.SetFloat("SPH_PMASS", SPH_PMASS);
-            particleCS.SetFloat("SPH_SIMSCALE", SPH_SIMSCALE);
-            particleCS.SetFloat("H", H);
-            particleCS.SetFloat("PI", PI);
-            particleCS.SetFloat("DT", DT);
-            particleCS.SetFloat("SPH_VISC", SPH_VISC);
-            particleCS.SetFloat("SPH_LIMIT", SPH_LIMIT);
-            particleCS.SetFloat("SPH_RADIUS", SPH_RADIUS);
-            particleCS.SetFloat("SPH_EPSILON", SPH_EPSILON);
-            particleCS.SetFloat("SPH_EXTSTIFF", SPH_EXTSTIFF);
-            particleCS.SetFloat("SPH_EXTDAMP", SPH_EXTDAMP);
-            particleCS.SetVector("MIN", MIN);
-            particleCS.SetVector("MAX", MAX);
-            particleCS.SetVector("GRAVITY", GRAVITY);
-            particleCS.SetFloat("Poly6Kern", Poly6Kern);
-            particleCS.SetFloat("SpikyKern", SpikyKern);
-            particleCS.SetFloat("LapKern", LapKern);
+            particleCS.SetFloat("_RestDensity", restDensity);
+            particleCS.SetFloat("_PressureStiff", pressureStiff);
+            particleCS.SetFloat("_PMass", p_mass);
+            particleCS.SetFloat("_SimScale", simulationScale);
+            particleCS.SetFloat("_SmoothStep", smoothStep);
+            particleCS.SetFloat("_DT", dt);
+            particleCS.SetFloat("_Viscosity", viscosity);
+            particleCS.SetFloat("_VelLimit", vel_limit);
+            particleCS.SetFloat("_Radius", radius);
+            particleCS.SetFloat("_WallStiff", wallStiff);
+            particleCS.SetFloat("_Restitution", restitution);
+            particleCS.SetVector("_Min", min);
+            particleCS.SetVector("_Max", max);
+            particleCS.SetVector("_Gravity", gravity);
+            particleCS.SetFloat("_DensityCoef", densityCoef);
+            particleCS.SetFloat("_GradPressureCoef", gradPressureCoef);
+            particleCS.SetFloat("_LapViscosityCoef", lapViscosityCoef);
             particleCS.SetVector("_GridDim", GridDim);
 
+            // Build Grid ----------------------------------------------------------------
             int kernel = particleCS.FindKernel("buildGrid");
-            particleCS.SetBuffer(kernel, "_ParticleBufferRead", particleBufferRead);
+            particleCS.SetBuffer(kernel, "_ParticleBufferRead", ParticleBufferRead);
             particleCS.SetBuffer(kernel, "_GridBufferRead", GridBufferRead);
             particleCS.SetBuffer(kernel, "_GridBufferWrite", GridBufferWrite);
             particleCS.Dispatch(kernel, threadGroupSize, 1, 1);
             SwapBuffer(ref GridBufferRead, ref GridBufferWrite);
 
-            HashGridKeyValue[] h = new HashGridKeyValue[numParticles];
-            GridBufferRead.GetData(h);
-            Debug.Log(h[10].key >> 16);
+            //HashGridKeyValue[] h = new HashGridKeyValue[numParticles];
+            //GridBufferRead.GetData(h);
+            //Debug.Log(h[10].key >> 16); // h[particle_id]:cell z
+            // Build Grid ----------------------------------------------------------------
 
             kernel = particleCS.FindKernel("sph");
-            particleCS.SetBuffer(kernel, "_ParticleBufferRead", particleBufferRead);
-            particleCS.SetBuffer(kernel, "_ParticleBufferWrite", particleBufferWrite);
+            particleCS.SetBuffer(kernel, "_ParticleBufferRead", ParticleBufferRead);
+            particleCS.SetBuffer(kernel, "_ParticleBufferWrite", ParticleBufferWrite);
             //particleCS.SetBuffer(kernel, "_GridSorted", GridSorted);
 
             particleCS.Dispatch(kernel, threadGroupSize, 1, 1);
 
-            SwapBuffer(ref particleBufferRead, ref particleBufferWrite);
+            SwapBuffer(ref ParticleBufferRead, ref ParticleBufferWrite);
         }
 
         void InitializeVariables() {
-            Poly6Kern = 315.0f / (64.0f * PI * Mathf.Pow(H, 9.0f));
-            SpikyKern = -45.0f / (PI * Mathf.Pow(H, 6.0f));
-            LapKern = 45.0f / (PI * Mathf.Pow(H, 6.0f));
+            densityCoef = 315.0f / (64.0f * Mathf.PI * Mathf.Pow(smoothStep, 9.0f));
+            gradPressureCoef = -45.0f / (Mathf.PI * Mathf.Pow(smoothStep, 6.0f));
+            lapViscosityCoef = 45.0f / (Mathf.PI * Mathf.Pow(smoothStep, 6.0f));
         }
 
         void InitializeComputeBuffer() {
-
-            // 箱の大きさに従ってパーティクルを生成
             List<Particle> particles = new List<Particle>();
             for (float x = 0; x <= numParticles; x++) {
-                particles.Add(new Particle((MAX + MIN)/2 + Random.insideUnitSphere * 10));
+                particles.Add(new Particle((max + min)/2 + Random.insideUnitSphere * 10));
             }
 
-            particleBufferRead = new ComputeBuffer(numParticles, Marshal.SizeOf(typeof(Particle)));    // パーティクル数を定義
-            particleBufferWrite = new ComputeBuffer(numParticles, Marshal.SizeOf(typeof(Particle)));
-            particleBufferRead.SetData(particles.ToArray());
+            ParticleBufferRead = new ComputeBuffer(numParticles, Marshal.SizeOf(typeof(Particle)));
+            ParticleBufferWrite = new ComputeBuffer(numParticles, Marshal.SizeOf(typeof(Particle)));
+            ParticleBufferRead.SetData(particles.ToArray());
 
             GridBufferRead = new ComputeBuffer(numParticles, Marshal.SizeOf(typeof(HashGridKeyValue)));
             GridBufferWrite = new ComputeBuffer(numParticles, Marshal.SizeOf(typeof(HashGridKeyValue)));
@@ -131,13 +124,13 @@ namespace SPH3D_FAST {
         }
 
         void ReleaseBuffer() {
-            if (particleBufferRead != null) {
-                particleBufferRead.Release();
-                particleBufferRead = null;
+            if (ParticleBufferRead != null) {
+                ParticleBufferRead.Release();
+                ParticleBufferRead = null;
             }
-            if (particleBufferWrite != null) {
-                particleBufferWrite.Release();
-                particleBufferWrite = null;
+            if (ParticleBufferWrite != null) {
+                ParticleBufferWrite.Release();
+                ParticleBufferWrite = null;
             }
             if (GridBufferRead != null) {
                 GridBufferRead.Release();
@@ -151,8 +144,11 @@ namespace SPH3D_FAST {
         
         void OnDrawGizmos() {
             Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube((max + min) / 2, max - min);
+        }
 
-            Gizmos.DrawWireCube((MAX + MIN) / 2, MAX - MIN);
+        void OnDestroy() {
+            ReleaseBuffer();
         }
 
     }
